82214c7ed3fe498166328ccae7463f90d8ebf78a
roothider <158083651+roothider@users.noreply.github.com>
2025-06-02 15:27:37 +0800

    add Instruments support for any process

--- README.md
+++ README.md
@@ -3,3 +3,8 @@
 Debug any process in Xcode.
 
 Tested on iOS 15.0/15.4/16.2/16.4/16.5.1/16.7.10/18.3 with palera1n, Dopamine and Dopamine (RootHide) jailbreaks.
+
+
+# How To Build
+
+https://github.com/roothide/Developer
--- build.sh
+++ build.sh
@@ -2,6 +2,6 @@
 
 set -e
 
-FINALPACKAGE=1 THEOS="$HOME/theos-roothide" THEOS_PACKAGE_SCHEME='' gmake clean package
-FINALPACKAGE=1 THEOS="$HOME/theos-roothide" THEOS_PACKAGE_SCHEME=rootless gmake clean package
-FINALPACKAGE=1 THEOS="$HOME/theos-roothide" THEOS_PACKAGE_SCHEME=roothide gmake clean package
+FINALPACKAGE=1 THEOS_PACKAGE_SCHEME='' gmake clean package
+FINALPACKAGE=1 THEOS_PACKAGE_SCHEME=rootless gmake clean package
+FINALPACKAGE=1 THEOS_PACKAGE_SCHEME=roothide gmake clean package
